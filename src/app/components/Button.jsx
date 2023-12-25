e29830864a4d7c06116d112ef19c0b8b2728f798
rodindima <rodindima1984@gmail.com>
2023-12-25 12:57:08 +0200

    feat: add new button component

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,5 +1,12 @@
-export default function Button() {
-    return (
-        <div className="">Button</div>
-    );
-}
+const Button = ({ onClick, children }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-purple hover:bg-vividBlue text-white p-4 rounded"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
