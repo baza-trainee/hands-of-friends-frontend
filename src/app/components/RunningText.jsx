3e9545274143086779cbd3b7675890a3541a485f
rodindima <rodindima1984@gmail.com>
2023-12-28 11:09:56 +0200

    feat:create-running-text git commit -m feat:create-running-text git commit -m feat:create-running-text

--- src/app/components/RunningText.jsx
+++ src/app/components/RunningText.jsx
@@ -1,5 +1,17 @@
-export default function RunningText() {
-    return (
-        <div className="">RunningText</div>
-    );
-}
+import Link from "next/link";
+import Marquee from "react-fast-marquee";
+
+const RunningText = () => {
+  return (
+    <Marquee autoFill={true} speed={40} pauseOnHover={true}>
+      <div className="flex text-2xl gap-x-10 mx-5 py-4">
+        <Link href="/">Разом до перемоги</Link>
+        <Link href="/">Допомога</Link>
+        <Link href="/">Підтримка</Link>
+        <Link href="/">Руки друзів </Link>
+      </div>
+    </Marquee>
+  );
+};
+
+export default RunningText;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import RunningText from "./components/RunningText";
 
 export default function Home() {
   return (
     <main className="">
       <Header />
+      <RunningText/>
       11111
       <Footer/>
     </main>
