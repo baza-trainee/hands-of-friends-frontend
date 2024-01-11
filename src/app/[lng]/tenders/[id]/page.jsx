1779c5b7d944383e11ae302ad2cfd591bd7c6212
RodinDima <rodindima1984@gmail.com>
2024-01-11 18:41:59 +0300

    feat:add-fix-Tenders-page

--- src/app/[lng]/tenders/[id]/page.jsx
+++ src/app/[lng]/tenders/[id]/page.jsx
@@ -1,27 +1,22 @@
-
 import Container from "@/app/components/Container";
-
 import Section from "@/app/components/Section";
 import { dataTenders } from "@/app/components/Tenders/data";
-
+import { useRouter } from "next/router";
 
 export default function Page() {
+  const router = useRouter();
+  const { id } = router.query;
+  const tender = dataTenders.find((item) => item.id === id);
 
-  // const router = useRouter();
-  // const { id } = router.query;
-  // const tender = dataTenders.find((item) => item.id === id);
-  console.log(dataTenders);
-
-  // if (!tender) {
-  //   return <p>Tender not found</p>;
-  // }
+  if (!tender) {
+    return <p>Tender not found</p>;
+  }
 
   return (
     <Container>
       <Section>
-       {111 && console.log(dataTenders)}
-       
-       
+        <p>{tender.performer}</p>
+    
       </Section>
     </Container>
   );
