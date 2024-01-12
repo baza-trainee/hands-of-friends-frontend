42c4eb9633c5b2b44f388e82efb60c9e391b3b5f
Rodin <Rodindima1984@gmail.com>
2024-01-12 20:47:26 +0200

    fix:fix-updated-component-breadcrumbs git commit -m fix:fix-updated-component-breadcrumbs git commit -m fix:fix-updated-component-breadcrumbs

--- src/app/components/BreadCrumbs.jsx
+++ src/app/components/BreadCrumbs.jsx
@@ -4,7 +4,7 @@ import Arrow from '@/assets/Frame.png'
 export default function BreadCrumbs({ href, text, className }) {
 	return (
 		<Link href={href}>
-			<div className={`flex gap-2 block ${className}`}>
+			<div className={`flex gap-2 ${className}`}>
 				<img src={Arrow.src} alt='arrow' />
 				<p className='text-xl text-deepBlue'>{text}</p>
 			</div>
