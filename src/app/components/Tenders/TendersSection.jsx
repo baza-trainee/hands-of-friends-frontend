4920669f85966d9b9eab9800d39c0edb1cca9a9c
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2024-01-14 15:34:35 +0200

    added the links and corrected the styles in tender section

--- src/app/components/Tenders/TendersSection.jsx
+++ src/app/components/Tenders/TendersSection.jsx
@@ -1,13 +1,21 @@
+"use client";
+import Button from "../Button";
+import { useRouter } from "next/navigation";
 import Title from "../Title";
 import TenderList from "./TendersList";
 import Section from "../Section";
 import Container from "../Container";
 export default function TendersSection() {
+   const router = useRouter();
     return (
       <Section>
         <Container>
           <Title titleName="Тендери" />
           <TenderList />
+          <Button onClick={() => router.push("/tenders")}
+         type="button"
+      className="block px-0 mx-auto mt-8 min-w-[12.38rem] font-normal bg-deepBlue border border-transparent hover:text-deepBlue hover:border hover:border-deepBlue"
+      >Дізнатись більше</Button>
         </Container>
       </Section>
     );
