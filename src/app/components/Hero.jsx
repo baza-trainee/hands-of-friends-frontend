24b23b75fac8e74419ec4e4b3e00c1d606008b59
RodinDima <rodindima1984@gmail.com>
2024-01-15 17:06:11 +0300

    feat/add-new-hero-component

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,52 +1,35 @@
 "use client";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
 import { CldVideoPlayer } from "next-cloudinary";
 import "next-cloudinary/dist/cld-video-player.css";
-
+import play from "../../assets/play.png";
 export default function Hero() {
   return (
     <>
-      {/* <ReactPlayer
-        controls="true"
-        width="100%"
-        height="100%"
-        url="https://res.cloudinary.com/dgkkn62i5/video/upload/1_hnfjx5.mp4"
-      />
-      <div className="flex justify-center">
-        <h2 className="absolute bottom-20 text-white text-4xl w-[799px]">
-          БО "Міжнародний благодійний фонд "Руки друзів" реалізує проєкти з
-          надання благодійної допомоги
-        </h2>
-      </div> */}
-
-      <CldVideoPlayer
-        logo={{
-          imageUrl:
-            "https://res.cloudinary.com/colbycloud-next-cloudinary/image/upload/v1676058142/assets/space-jelly-cosmo-helmet.svg",
-          onClickUrl: "https://spacejelly.dev",
-        }}
-        width="1280"
-        height="650"
-        src="1_hnfjx5"
-      />
-      <div className="flex justify-center mb-20">
-        <h2 className="absolute bottom-24 text-white text-4xl w-[799px]">
-          БО "Міжнародний благодійний фонд "Руки друзів" реалізує проєкти з
-          надання благодійної допомоги
-        </h2>
+      <div className="relative mb-20">
+        {/* <img
+          className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none	"
+          src={play.src}
+          alt="icon"
+        /> */}
+        <CldVideoPlayer width="1280" height="650" src="1_hnfjx5" />
+        <div className="flex justify-center">
+          <h2 className="absolute bottom-24 text-white text-4xl w-[799px]">
+            БО "Міжнародний благодійний фонд "Руки друзів" реалізує проєкти з
+            надання благодійної допомоги
+          </h2>
+        </div>
       </div>
 
       <iframe
         width="1280"
         height="650"
-        src="https://www.youtube.com/embed/UYXcryt21m8?rel=0&controls=0&modestbranding=1"
+        src="https://www.youtube.com/embed/UYXcryt21m8?rel=0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
       ></iframe>
-      <div className="flex justify-center mb-20">
-        <h2 className="absolute bottom-24 text-white text-4xl w-[799px]">
-          БО "Міжнародний благодійний фонд "Руки друзів" реалізує проєкти з
-          надання благодійної допомоги
-        </h2>
-      </div>
     </>
   );
 }
