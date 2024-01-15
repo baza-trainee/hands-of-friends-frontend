26174e485359ec4d2f159a32eeaaeec35e825e22
Viktor Filippov <jackal3166@gmail.com>
2024-01-15 21:15:44 +0200

    Return ProjectList

--- src/app/components/Projects/ProjectList.jsx
+++ src/app/components/Projects/ProjectList.jsx
@@ -1,39 +1,35 @@
-import Button from "../Button";
+import Action from "../Action";
 import { dataProjects } from "./data";
 
 export default function ProjectList() {
-    return (
-        <>
-        <ul className="flex gap-5 not-italic leading-normal">
+  return (
+    <>
+      <ul className="flex gap-5 not-italic leading-normal">
         {dataProjects.map((item) => (
-       
-       <li
-         key={item.id}
-         className="flex flex-col min-w-[22.5rem]"
-       >
-        
-        {/* <div className="flex justify-between text-lg font-body"> */}
-        <h3 className="mb-1 text-2xl font-body">{item.name}</h3>
-        <span className={`font-bold text-lg ${
-            item.type === 'Активний'
-            ? 'text-green'
-            : 'text-lightGray'
-        }`}
-        >
-        {item.type}
-        </span>
-{/* 
+          <li key={item.id} className="flex flex-col min-w-[22.5rem]">
+            {/* <div className="flex justify-between text-lg font-body"> */}
+            <h3 className="mb-1 text-2xl font-body">{item.name}</h3>
+            <span
+              className={`font-bold text-lg ${
+                item.type === "Активний" ? "text-green" : "text-lightGray"
+              }`}
+            >
+              {item.type}
+            </span>
+            {/* 
         </div> */}
-         <p className="mt-6 text-lg font-medium leading-6 text-left text-black font-body">
-           {item.description}
-         </p>
-       
-       </li>   
-         ))}
-        </ul>
-        <Button
-           type="button"
+            <p className="mt-6 text-lg font-medium leading-6 text-left text-black font-body">
+              {item.description}
+            </p>
+          </li>
+        ))}
+      </ul>
+      <Action
+        type="button"
         className="block px-0 mx-auto mt-8 min-w-[13.63rem] font-normal bg-deepBlue border border-transparent hover:text-deepBlue hover:border hover:border-deepBlue"
-        >Завантажити більше</Button>
-        </>
-    )}
+      >
+        Завантажити більше
+      </Action>
+    </>
+  );
+}
