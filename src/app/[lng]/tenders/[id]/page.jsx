28e252fe80225a03c37e67e42c74a944c1cab844
rodindima <rodindima1984@gmail.com>
2024-01-19 14:52:41 +0200

    feat/add-axios-to-tenders-page

--- src/app/[lng]/tenders/[id]/page.jsx
+++ src/app/[lng]/tenders/[id]/page.jsx
@@ -1,77 +1,46 @@
-import BreadCrumbs from '@/app/components/BreadCrumbs'
-import Container from '@/app/components/Container'
-import Section from '@/app/components/Section'
-import { dataTenders } from '@/app/components/Tenders/data'
-import Link from 'next/link'
+"use client";
+import BreadCrumbs from "@/app/components/BreadCrumbs";
+import Container from "@/app/components/Container";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+async function getData() {
+  try {
+    const response = await axios.get(
+      `https://hands-of-friends-backend.onrender.com/api/content_management/tenders/${id}`
+    );
+    return response.data.results;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
 
 export default function Page({ params }) {
-	const { id } = params
-	const Tender = dataTenders.find(tender => tender.id === id)
+  const [data, setData] = useState([]);
 
-	return (
-		<>
-			<Container>
-				<BreadCrumbs href='/tenders' className='my-12 ' text='Тендери' />
+  useEffect(() => {
+    async function fetchData() {
+      const tenderPromise = getData();
 
-				<div className='max-w-[835px] text-lg'>
-					<p className='mb-6 '>{Tender.data}</p>
-					<h2 className='text-3xl	mb-10'>{Tender.text}</h2>
-					<p className='mb-6'>{Tender.desc}</p>
-					<div
-						className='flex gap-1
-        '
-					>
-						<p>Дата початку:</p>
-						<p className='font-bold	'>{Tender.start}</p>
-					</div>
-					<div className='flex gap-1 mb-8'>
-						<p>Дата закінчення: </p>
-						<p className='font-bold	'>{Tender.end}</p>
-					</div>
-					<p className='text-2xl font-bold mb-6'>{Tender.lotNumberOne}</p>
-					<p className='mb-4'>{Tender.lotDesc}</p>
-					<p className='font-bold'>{Tender.titleLink}</p>
-					<Link className='block mb-8' href={Tender.link}>
-						ПОСИЛАННЯ
-					</Link>
-					<p className='text-2xl font-bold mb-6'>{Tender.lotNumberTwo}</p>
-					<p className='mb-4'>{Tender.lotDesc}</p>
-					<p className='font-bold'>{Tender.titleLink}</p>
-					<Link className='block mb-8' href={Tender.link}>
-						ПОСИЛАННЯ
-					</Link>
-					<p className='mb-6'>{Tender.partners}</p>
-					<ol>
-						<li>
-							<Link className='block mb-4' href='#'>
-								ГО Фонд громади Харкова "Толока"{' '}
-							</Link>
-						</li>
-						<li>
-							<Link className='block mb-4' href='#'>
-								ГО Фонд громади Вознесенська у партнерстві з міжнародною
-								організацією Plan International
-							</Link>
-						</li>
-						<li>
-							<Link className='block mb-4' href='#'>
-								Проєкт фінансується Міською радою міста Мадрид (Іспанія).{' '}
-							</Link>
-						</li>
-					</ol>
+      const [tenderData] = await Promise.all([tenderPromise]);
+      setData(tenderData);
+    }
+    fetchData();
+  }, []);
+  console.log("data", data);
 
-					<p className='mb-8 max-w-[740px]'>{Tender.partnersText}</p>
-					<p className='font-bold mb-6'>{Tender.contacts}</p>
-					<ul>
-						<li className='mb-4'>{Tender.email}</li>
-						<li className='mb-4'>{Tender.phone}</li>
-					</ul>
-					<div className='flex gap-1'>
-						<p>Відповідальним виконавець: </p>
-						<p className='font-bold	'>{Tender.performer}</p>
-					</div>
-				</div>
-			</Container>
-		</>
-	)
+  return (
+    <>
+      <Container>
+        <BreadCrumbs href="/tenders" className="my-12 " text="Тендери" />
+
+        <div className="max-w-[835px] text-lg">
+          <p>{params.id}</p>
+          <p>{data.title}</p>
+        </div>
+      </Container>
+    </>
+  );
 }
