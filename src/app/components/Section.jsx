c7a7da8196e872b52c80a178b264cba22078dae8
dav1dashka <dujadavyd17@gmail.com>
2024-01-24 14:02:12 +0200

    fix: changed name of files from js to jsx
    - changed name of files from js to jsx
    - changed name of props from marginBottom and marginTop to marginbottom margintop to resolve error
    - fixed logic of langSwithcer but not styles
    - commented not used code

--- src/app/components/Section.jsx
+++ src/app/components/Section.jsx
@@ -1,7 +1,7 @@
 export default function Section(props) {
-  const sectionClass = `font-body mt-[3.75rem] xl:mt-36 2xl:mt-40${
-    props.marginBottom ? props.marginBottom : ""
-  } ${props.marginTop ? props.marginTop : ""}`;
+  const sectionClass = `font-body mt-[3.75rem] xl:mt-36 2xl:mt-40
+    ${props.marginbottom ? props.marginbottom : ""} 
+    ${props.margintop ? props.margintop : ""}`;
 
   return <section className={sectionClass} {...props} />;
 }
