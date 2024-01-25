136482f77fcd8db2ce30716299cb7879c12ba251
rodindima <rodindima1984@gmail.com>
2024-01-25 12:08:50 +0200

    refactor/rewrite-hero-section

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,43 +1,52 @@
 "use client";
 
 import { useState } from "react";
-import { Hourglass } from "react-loader-spinner";
-import { CldVideoPlayer } from "next-cloudinary";
-import Container from "./Container";
+//import { Hourglass } from "react-loader-spinner";
+//import { CldVideoPlayer } from "next-cloudinary";
 
-import "next-cloudinary/dist/cld-video-player.css";
+import Section from "./Section";
+
+//import "next-cloudinary/dist/cld-video-player.css";
 
 export default function Hero() {
-  const [videoLoaded, setVideoLoaded] = useState(false);
-  const handleVideoLoaded = () => {
-    setVideoLoaded(true);
-  };
+  //const [videoLoaded, setVideoLoaded] = useState(false);
+  //const handleVideoLoaded = () => {
+  //  setVideoLoaded(true);
+  //};
   return (
     <>
-      <Container>
+      <Section className="mt-0 max-w-[1440px] mx-auto">
         <div className="relative mb-20">
-          {!videoLoaded && (
+          {/*{!videoLoaded && (
             <div className="absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white">
               <Hourglass
                 visible={true}
-                height="80"
-                width="80"
+                height="40"
+                width="40"
                 ariaLabel="hourglass-loading"
                 wrapperStyle={{}}
                 wrapperClass=""
                 colors={["#306cce", "#72a1ed"]}
               />
             </div>
-          )}
-          <div className="">
+          )}*/}
+          {/*<div className="h-[869px] ">
             <CldVideoPlayer
-              poster="https://res.cloudinary.com/dgkkn62i5/image/upload/v1705565089/hero_mvqyzh.jpg"
-              width="1440"
-              height="869"
-              src="1_hnfjx5"
+              poster="https://res.cloudinary.com/dgkkn62i5/image/upload/v1706174676/111_wlvg1h.png"
+              width="1440%"
+              height="100%"
+              src="test_video"
               onDataLoad={handleVideoLoaded}
             />
-          </div>
+          </div>*/}
+          <iframe
+            src="https://player.cloudinary.com/embed/?public_id=test_video&cloud_name=dgkkn62i5&player[seekThumbnails]=true&player[hideContextMenu]=true&player[floatingWhenNotVisible]=right&player[posterOptions][transformation][startOffset]=1"
+            width="1440"
+            height="869"
+            allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+            allowfullscreen
+            frameborder="1"
+          ></iframe>
           <div className="flex justify-center">
             <h2 className="absolute bottom-24 text-white text-4xl w-[799px]">
               БО "Міжнародний благодійний фонд "Руки друзів" реалізує проєкти з
@@ -45,17 +54,25 @@ export default function Hero() {
             </h2>
           </div>
         </div>
+        <div className="relative"></div>
+      </Section>
 
+      <div className="max-w-[1440px] mx-auto relative">
         <iframe
-          width="1280"
-          height="650"
+          width="1440"
+          height="869"
           src="https://www.youtube.com/embed/UYXcryt21m8?rel=0"
           title="YouTube video player"
-          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
-      </Container>
+        <div className="flex justify-center">
+          <h2 className="absolute bottom-24 text-white text-4xl w-[799px]">
+            БО "Міжнародний благодійний фонд "Руки друзів" реалізує проєкти з
+            надання благодійної допомоги
+          </h2>
+        </div>
+      </div>
     </>
   );
 }
