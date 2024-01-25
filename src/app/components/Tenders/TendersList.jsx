2e7df335aa32e3de02e3a718061dec8f27d42fd2
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-01-25 17:00:59 +0100

    feat: add loading and update tenders

--- src/app/components/Tenders/TendersList.jsx
+++ src/app/components/Tenders/TendersList.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import getFetchedData from "@/app/helpers/helperAPI";
 import Skeleton from "./Skeleton";
+import { useHttp } from "@/app/hooks/useHttp";
 
 // import { dataTenders } from "./data";
 
@@ -20,47 +21,52 @@ import Skeleton from "./Skeleton";
 // }
 
 export default function TenderList() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
-  let skeleton = [... new Array(3)].map((_, i) => (<Skeleton key={i} className="bg-zinc-200" />));
+  // const [data, setData] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  let skeleton = [...new Array(3)].map((_, i) => (
+    <Skeleton key={i} className="bg-zinc-200" />
+  ));
+  const [data, setData, isLoading] = useHttp("tenders/");
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     setIsLoading(true);
+  //     const tenderPromise = getFetchedData('tenders/', { "Accept-Language": "uk" });
 
-  useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
-      const tenderPromise = getFetchedData('tenders/', { "Accept-Language": "uk" });
-
-      const [tenderData] = await Promise.all([tenderPromise]);
-      setData(tenderData.slice(0, 3));
-      // console.log(tenderData);
-    }
-    fetchData();
-    setIsLoading(false);
-  }, []);
+  //     const [tenderData] = await Promise.all([tenderPromise]);
+  //     setData(tenderData.slice(0, 3));
+  //     // console.log(tenderData);
+  //   }
+  //   fetchData();
+  //   setIsLoading(false);
+  // }, []);
 
   return (
     <>
       <ul className="flex gap-5 not-italic leading-normal">
-        {isLoading ? skeleton :
-          data.map((item) => (
-            <li
-              key={item.id}
-              className="flex flex-col p-9 min-w-[22.5rem] bg-[#E0F2FE]"
-            >
-              <Link href={`/tenders/${item.id}`}>
-                <div className="flex justify-between font-body text-lg">
-                  <span
-                    className={`font-bold text-lg ${item.is_active ? "text-green" : "text-lightGray"}`}
-                  >
-                    {item.is_active ? "Активний" : "Архівний"}
-                  </span>
-                  <span className="text-black">{item.date}</span>
-                </div>
-                <p className="font-sans mt-6 text-left text-2xl text-black font-medium">
-                  {item.title}
-                </p>
-              </Link>
-            </li>
-          ))}
+        {isLoading
+          ? skeleton
+          : data.map((item) => (
+              <li
+                key={item.id}
+                className="flex flex-col p-9 min-w-[22.5rem] bg-[#E0F2FE]"
+              >
+                <Link href={`/tenders/${item.id}`}>
+                  <div className="flex justify-between font-body text-lg">
+                    <span
+                      className={`font-bold text-lg ${
+                        item.is_active ? "text-green" : "text-lightGray"
+                      }`}
+                    >
+                      {item.is_active ? "Активний" : "Архівний"}
+                    </span>
+                    <span className="text-black">{item.date}</span>
+                  </div>
+                  <p className="font-sans mt-6 text-left text-2xl text-black font-medium">
+                    {item.title}
+                  </p>
+                </Link>
+              </li>
+            ))}
       </ul>
     </>
   );
--- src/app/hooks/useHttp.jsx
+++ src/app/hooks/useHttp.jsx
@@ -4,17 +4,19 @@ import getFetchedData from "../helpers/helperAPI";
 
 export const useHttp = (endpoint) => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const lang = useCurrentLang();
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const response = await getFetchedData(endpoint, {
         "Accept-Language": lang,
       });
-
+      setIsLoading(false);
       setData(response);
     }
     fetchData();
   }, [endpoint, lang]);
-  return [data, setData];
+  return [data, setData, isLoading];
 };
