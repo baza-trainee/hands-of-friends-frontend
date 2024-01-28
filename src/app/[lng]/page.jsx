7e4426a944b6e36507a2e23742e9d0663aec3e54
dav1dashka <dujadavyd17@gmail.com>
2024-01-28 12:06:27 +0200

    test
    testing rerendering page

--- src/app/[lng]/page.jsx
+++ src/app/[lng]/page.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import About from '../components/About';
 import CooperationSection from '../components/Cooperation/CooperationSection';
 import FeedbackFormSection from '../components/FeedbackForm/FeedbackFormSection';
@@ -8,6 +10,8 @@ import TeamSection from '../components/Team/TeamSection';
 import TendersSection from '../components/Tenders/TendersSection';
 
 export default async function Page() {
+	console.log(1)
+
 	return (
 		<>
 			<Hero />
