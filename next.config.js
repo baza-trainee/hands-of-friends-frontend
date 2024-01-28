28f835b8a06b5ea96c224a4dbb1fad685e69b1e5
dav1dashka <dujadavyd17@gmail.com>
2024-01-28 10:40:50 +0200

    refactor: add svgr package, change svg to svgr

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["hands-of-friends-backend.onrender.com"],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
