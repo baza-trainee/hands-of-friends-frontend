1c70b1b12a3419c89f936da08b3c3cb61cc75da4
rodindima <rodindima1984@gmail.com>
2024-01-29 15:24:36 +0200

    feat/add-privacy-add-terms-page

---
+++ src/app/[lng]/privacy-policy/page.jsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <>
+      <iframe
+        src={"/ua/docs/Privacy policy.pdf" + "#toolbar=0"}
+        type="application/pdf"
+        className="w-full h-screen border-none"
+      />
+    </>
+  );
+}
