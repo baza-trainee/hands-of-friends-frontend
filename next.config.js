82fb37c731fa92c0d9bb909cccbb9a617eaaceac
dav1dashka <dujadavyd17@gmail.com>
2024-01-30 17:04:32 +0200

    Revert "refactor: add rules in eslint config"
    This reverts commit 15b8c77f4d219447a363e34d3d73fa22a99ecbda.

--- next.config.js
+++ next.config.js
@@ -1,24 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ['hands-of-friends-backend.onrender.com'],
-	},
-	webpack: config => {
-		config.module.rules.push({
-			test: /\.(pdf)$/,
-			use: [
-				{
-					loader: 'file-loader',
-					options: {
-						publicPath: '/_next',
-						outputPath: 'static/images',
-						name: '[name].[ext]',
-					},
-				},
-			],
-		})
-		return config
-	},
-}
+  images: {
+    domains: ["hands-of-friends-backend.onrender.com"],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
