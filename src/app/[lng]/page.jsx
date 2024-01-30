40fe6cebc4a76e7c4318375939698aca19074948
dav1dashka <dujadavyd17@gmail.com>
2024-01-30 17:34:15 +0200

    fix: Revert "Revert "refactor: add rules in eslint config""
    This reverts commit 82fb37c731fa92c0d9bb909cccbb9a617eaaceac.

--- src/app/[lng]/page.jsx
+++ src/app/[lng]/page.jsx
@@ -1,5 +1,3 @@
-'use client';
-
 import About from '../components/About';
 import CooperationSection from '../components/Cooperation/CooperationSection';
 import FeedbackFormSection from '../components/FeedbackForm/FeedbackFormSection';
@@ -10,8 +8,6 @@ import TeamSection from '../components/Team/TeamSection';
 import TendersSection from '../components/Tenders/TendersSection';
 
 export default async function Page() {
-	console.log(1)
-
 	return (
 		<>
 			<Hero />
