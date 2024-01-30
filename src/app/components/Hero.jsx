15b8c77f4d219447a363e34d3d73fa22a99ecbda
dav1dashka <dujadavyd17@gmail.com>
2024-01-30 16:22:12 +0200

    refactor: add rules in eslint config

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,73 +1,79 @@
-"use client";
-import { useState } from "react";
+'use client'
+import { useState, useRef } from 'react'
 
-import Image from "next/image";
+import Image from 'next/image'
 
-import Section from "./Section";
-
-import Img from "../../../public/img/play-icon.svg";
+import Section from './Section'
+import Img from '../../../public/img/play-icon.svg'
 
 export default function Hero() {
-  const [isVideoPlaying, setVideoPlaying] = useState(false);
-  const handleVideoPlay = () => {
-    setVideoPlaying(true);
-  };
+	const [isVideoPlaying, setVideoPlaying] = useState(false)
+	const [isVideoPaused, setVideoPaused] = useState(false)
+	const [isSeeking, setSeeking] = useState(false) 
+	const videoRef = useRef(null)
 
-  const handleVideoPause = () => {
-    setVideoPlaying(false);
-  };
+	const handleVideoPlay = () => {
+		setVideoPlaying(true)
+		setVideoPaused(false)
+	}
 
-  return (
-    <>
-      <Section className="mt-0 max-w-[1440px] mx-auto">
-        <div className="relative mb-20">
-          <div className="video-container relative">
-            <div className="video-container relative mt-0 max-w-[1440px] mx-auto">
-              <video
-                width="100%"
-                height="100%"
-                src="https://res.cloudinary.com/dgkkn62i5/video/upload/v1706166292/test_video.mp4"
-                controls
-                poster="https://res.cloudinary.com/dgkkn62i5/image/upload/v1706174676/111_wlvg1h.png"
-                onPlay={handleVideoPlay}
-                onPause={handleVideoPause}
-              ></video>
+	const handleVideoPause = () => {
+		setVideoPlaying(false)
+		setVideoPaused(true)
+	}
 
-              {!isVideoPlaying && (
-                <div
-                  className="play-icon absolute inset-0 flex items-center justify-center "
-                  style={{ pointerEvents: "none" }}
-                >
-                  <Image src={Img} width={128} height={171} alt="Play" />
-                </div>
-              )}
-            </div>
-            <div className="flex justify-center">
-              <h2 className="absolute bottom-24 text-white text-4xl w-[799px]">
-                БО "Міжнародний благодійний фонд "Руки друзів" реалізує проєкти
-                з надання благодійної допомоги
-              </h2>
-            </div>
-          </div>
-        </div>
-      </Section>
+	const handlePlayClick = () => {
+		if (videoRef.current) {
+			setVideoPlaying(true)
+			videoRef.current.play()
+		}
+	}
 
-      <div className="max-w-[1440px] mx-auto relative">
-        <iframe
-          width="1440"
-          height="869"
-          src="https://www.youtube.com/embed/UYXcryt21m8?rel=0"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-        <div className="flex justify-center">
-          <h2 className="absolute bottom-24 text-white text-4xl w-[799px]">
-            БО "Міжнародний благодійний фонд "Руки друзів" реалізує проєкти з
-            надання благодійної допомоги
-          </h2>
-        </div>
-      </div>
-    </>
-  );
+	const handleVideoSeeked = () => {
+		if (isSeeking && isVideoPaused) {
+			setVideoPlaying(true)
+			setSeeking(false) 
+			videoRef.current.play()
+		}
+	}
+
+	const handleVideoSeeking = () => {
+		setSeeking(true) 
+	}
+
+	return (
+		<>
+			<Section className='mt-0 max-w-[1440px] mx-auto'>
+				<div className='relative mb-20'>
+					<div className='video-container relative'>
+						<div className='video-container relative mt-0 max-w-[1440px] mx-auto'>
+							<video
+								ref={videoRef}
+								width='100%'
+								height='100%'
+								src='https://res.cloudinary.com/dgkkn62i5/video/upload/v1706166292/test_video.mp4'
+								poster='https://res.cloudinary.com/dgkkn62i5/image/upload/v1706174676/111_wlvg1h.png'
+								preload='auto'
+								onPlay={handleVideoPlay}
+								onPause={handleVideoPause}
+								onSeeked={handleVideoSeeked}
+								onSeeking={handleVideoSeeking}
+								autoPlay={isVideoPlaying}
+								muted={isVideoPlaying}
+								controls={isVideoPlaying}
+							></video>
+						</div>
+						{(isVideoPaused || !isVideoPlaying) && !isSeeking && (
+							<div
+								className='play-icon absolute inset-0 flex items-center justify-center'
+								onClick={handlePlayClick}
+							>
+								<Image src={Img} width={128} height={171} alt='Play' />
+							</div>
+						)}
+					</div>
+				</div>
+			</Section>
+		</>
+	)
 }
