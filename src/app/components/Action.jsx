d45d69b97c94f35dbe317e0a25e22c74c314caf6
rodindima <rodindima1984@gmail.com>
2024-02-02 15:30:39 +0200

    fix/fix-style-action-cookies-panel

--- src/app/components/Action.jsx
+++ src/app/components/Action.jsx
@@ -1,14 +1,21 @@
 import Link from "next/link";
 import clsx from "clsx";
 
-export default function Action({ onClick, children, className, href, rel, target }) {
+export default function Action({
+  onClick,
+  children,
+  className,
+  href,
+  rel,
+  target,
+}) {
   const Action = href ? Link : "button";
   const additionalProps = Action === Link ? { href, rel, target } : { onClick };
 
   return (
     <Action
       className={clsx(
-        "py-4 transition font-body text-white text-xl font-normal rounded-md border-2 border-transparent hover:bg-transparent hover:border-solid hover:border-2",
+        "py-4 transition font-body text-white text-xl font-normal rounded-md border-2 hover:bg-transparent hover:border-solid hover:border-2",
         className
       )}
       {...additionalProps}
@@ -16,4 +23,4 @@ export default function Action({ onClick, children, className, href, rel, target
       {children}
     </Action>
   );
-};
+}
