36c837323c82ee61378b9866f26beec4f759ad0b
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-02-03 12:21:57 +0100

    feat: updated adaptive styles

--- src/app/components/Cooperation/CooperationList.jsx
+++ src/app/components/Cooperation/CooperationList.jsx
@@ -3,23 +3,39 @@ import { dataCooperation } from "./data";
 
 export default function CooperationList() {
   return (
-    <ul className="flex gap-5 font-body  text-black">
+    <ul
+      className="flex flex-col justify-center items-center gap-5 font-body  text-black
+      sm:gap-8
+      md:flex-row md:flex-wrap md:gap-5"
+    >
       {dataCooperation.map((item, index) => (
         <li
           key={index}
-          className="flex flex-col items-center p-12 min-w-[22.5rem] bg-[#E0F2FE]"
+          className="flex flex-col grow items-center px-[1.13rem] py-[1.63rem] max-w-[18rem] bg-[#E0F2FE]
+          md:px-6 md:py-8 md:max-w-[20.88rem]
+          lg:py-12 lg:px-8 lg:max-w-[22.5rem]
+          2xl:py-14 2xl:px-11 2xl:max-w-[24rem]"
         >
-          <h3 className="mb-6 font-body font-bold text-3xl text-deepBlue">
+          <h3
+            className="mb-6 font-body text-2xl font-medium text-deepBlue
+          md:text-3xl md:font-bold leading-8
+          xl:leading-9"
+          >
             {item.subtitle}
           </h3>
-          <p className="grow mb-8 text-lg italic font-light text-center">
+          <p
+            style={index === 1 ? { maxWidth: "188px" } : { maxWidth: "251px" }}
+            className="mb-6 grow text-base italic font-light text-center
+            md:mb-5 md:text-lg
+            xl:mb-8"
+          >
             {item.text}
           </p>
           <Action
             href={"https://docs.google.com/forms/u/0/"}
             rel="noopener noreferrer"
             target="_blank"
-            className=" px-0 min-w-[12.38rem]  bg-deepBlue text-center hover:text-deepBlue  hover:border-deepBlue"
+            className="px-0 min-w-[12.38rem]  bg-deepBlue text-center border-0 hover:text-deepBlue  hover:border-deepBlue"
           >
             {item.btnText}
           </Action>
--- src/app/components/Title.jsx
+++ src/app/components/Title.jsx
@@ -1,6 +1,9 @@
 export default function Title({ titleName }) {
   return (
-    <h2 className="mb-8 xl:mb-[3.78rem] font-body font-bold  text-black text-2xl  xl:text-3xl  pseudo ">
+    <h2
+      className="mb-8 font-body font-bold  text-black text-2xl pseudo
+    xl:mb-[3.78rem] xl:text-3xl"
+    >
       {titleName}
     </h2>
   );
