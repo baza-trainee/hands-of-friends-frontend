daeb943384fe1910f05962809b1537bf97915e8a
dav1dashka <dujadavyd17@gmail.com>
2024-02-03 15:15:46 +0200

    refactor: edited styles for container
    Also deleated unused components

--- src/app/components/LangSwitcher.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function LangSwitcher() {
-    return (
-        <div className="">LangSwitcher</div>
-    );
-}
--- src/app/components/RunningText.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function RunningText() {
-    return (
-        <div className="">RunningText</div>
-    );
-}
