d20a6d85d99ff8e93f8fe29b871ad29f6be396a7
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-02-05 13:45:29 +0100

    fix: conflicts resolve

--- src/app/[lng]/page.jsx
+++ src/app/[lng]/page.jsx
@@ -1,34 +1,14 @@
 import React from "react";
-import About from "../components/About";
-import CooperationSection from "../components/Cooperation/CooperationSection";
-import FeedbackFormSection from "../components/FeedbackForm/FeedbackFormSection";
 import Hero from "../components/Hero";
-import News from "../components/News";
-import ProjectSection from "../components/Projects/ProjectSection";
-import TeamSection from "../components/Team/TeamSection";
-import TendersSection from "../components/Tenders/TendersSection";
 import About from "../components/About";
-import CooperationSection from "../components/Cooperation/CooperationSection";
-import FeedbackFormSection from "../components/FeedbackForm/FeedbackFormSection";
-import Hero from "../components/Hero";
-import News from "../components/News";
 import ProjectSection from "../components/Projects/ProjectSection";
-import TeamSection from "../components/Team/TeamSection";
 import TendersSection from "../components/Tenders/TendersSection";
+import TeamSection from "../components/Team/TeamSection";
+import CooperationSection from "../components/Cooperation/CooperationSection";
+import News from "../components/News";
+import FeedbackFormSection from "../components/FeedbackForm/FeedbackFormSection";
 
 export default async function Page() {
-  return (
-    <>
-      <Hero />
-      <About />
-      <ProjectSection />
-      <TendersSection />
-      <TeamSection />
-      <CooperationSection />
-      <News />
-      <FeedbackFormSection />
-    </>
-  );
   return (
     <>
       <Hero />
