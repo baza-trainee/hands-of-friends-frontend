3bb719ed8b7d744bfad7be5ccd2e4dcf18074fcb
rodindima <rodindima1984@gmail.com>
2024-02-05 15:26:26 +0200

    feat/create-aboutUs-page

--- src/app/[lng]/aboutUs/page.jsx
+++ src/app/[lng]/aboutUs/page.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import Link from 'next/link';
-import { useTranslation } from '../../i18n';
+"use client";
+import Container from "@/app/components/Container";
+import BreadCrumbs from "@/app/components/BreadCrumbs";
+import AboutTabs from "../../components/About/AboutTabs";
+import AboutContent from "../../components/About/AboutContent";
+import { useState } from "react";
 
-export default async function Page({ params: { lng } }) {
-	const { t } = await useTranslation(lng, 'aboutUs');
+function Page() {
+  const [activeTab, setActiveTab] = useState("mission");
 
-	return (
-		<>
-			<h1>{t('title')}</h1>
-			<Link href={`/${lng}`}>{t('back-to-home')}</Link>
-		</>
-	);
+  return (
+    <div>
+      <div className="bg-image-tenders bg-center min-h-[620px]">
+        <Container>
+          <BreadCrumbs
+            className="mb-[319px] z-10"
+            href="/"
+            text="Назад"
+            textColor="white"
+          />
+          <h2 className="text-3xl text-white font-bold mb-16">Про нас</h2>
+          <AboutTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        </Container>
+      </div>
+      <AboutContent activeTab={activeTab} />
+    </div>
+  );
 }
+
+export default Page;
