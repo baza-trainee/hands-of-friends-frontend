b0c24a6622aadd638b0c923698eca4124d748bcf
rodindima <rodindima1984@gmail.com>
2024-02-05 16:49:57 +0200

    bugfix/create-component-layout

---
+++ src/app/components/Layout.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+const Layout = ({ children, lng }) => {
+  const pathname = usePathname();
+  const isTest = pathname.split("/").includes("privacy-policy");
+
+  return (
+    <>
+      {!isTest && (
+        <header>
+          <Header lng={lng} />
+        </header>
+      )}
+      <main>{children}</main>
+      {!isTest && (
+        <footer>
+          <Footer lng={lng} />
+        </footer>
+      )}
+    </>
+  );
+};
+
+export default Layout;
