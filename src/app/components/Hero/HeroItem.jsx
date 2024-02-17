f42fc245e82238b3fc85e28a54708b0ab217b065
Rodin <Rodindima1984@gmail.com>
2024-02-17 20:28:57 +0200

    fix/fix-style-hero-pagination

--- src/app/components/Hero/HeroItem.jsx
+++ src/app/components/Hero/HeroItem.jsx
@@ -9,8 +9,9 @@ export default function HeroItem({ data }) {
 				alt={`${data.image}`}
 				width={1440}
 				height={869}
-				className='w-auto md:h-[700px] xl:h-[832px] 2xl:h-[976px] '
+				className='w-auto h-[1024px] md:h-[1024px] xl:h-[832px] 2xl:h-[976px] '
 			/>
+
 			<p className='absolute left-1/2 transform -translate-x-1/2 bottom-20 mx-0 text-white text-[20px] lg:w-[881px] lg:text-[40px] xl:w-[1175px] '>
 				{data.title}
 			</p>
