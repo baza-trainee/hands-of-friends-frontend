da7d7116d25743f95562864047e673731da9f5e5
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-02-19 16:54:32 +0100

    feat: updated cooperatio section

--- src/app/components/Team/TeamItems.jsx
+++ src/app/components/Team/TeamItems.jsx
@@ -14,10 +14,10 @@ export default function TeamItem({ data }) {
         height={290}
         className="mb-3 h-[290px] object-cover "
       />
-      <h3 className="w-[219px] text-2xl text-center capitalize ">
+      <h3 className="w-[219px] text-2xl text-center font-bold capitalize ">
         {name.firstName}
       </h3>
-      <h3 className=" mb-2 w-[219px] text-2xl text-center capitalize  overflow-hidden truncate hover:overflow-visible hover:whitespace-normal grow break-words">
+      <h3 className=" mb-2 w-[219px] text-2xl text-center font-bold capitalize  overflow-hidden truncate hover:overflow-visible hover:whitespace-normal grow break-words">
         {name.lastName}
       </h3>
       <p className="text-lg w-[219px] text-center capitalize">
--- src/app/components/Title.jsx
+++ src/app/components/Title.jsx
@@ -5,7 +5,7 @@ export default function Title({ titleName, className }) {
   return (
     <h2
       className={clsx(
-        `mb-8 font-body font-bold  text-black text-2xl pseudo
+        `mb-8 font-body font-bold  text-black text-2xl  antialiased pseudo
         sm:mb-10
         md:mb-14 
         xl:mb-[3.77rem] xl:text-3xl`,
