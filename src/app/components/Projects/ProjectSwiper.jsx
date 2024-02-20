98eaa5a7657132b89d8d70d12b162e65ff098e71
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2024-02-20 12:10:29 +0200

    adaptive

--- src/app/components/Projects/ProjectSwiper.jsx
+++ src/app/components/Projects/ProjectSwiper.jsx
@@ -12,9 +12,9 @@ export default function ProjectSwiperList() {
         ItemComponent={ProjectItem}
         swiperSettings={{
           breakpoints: {
-            // 320: { slidesPerView: 1.15, spaceBetween: 16 },
-            // 420: { slidesPerView: 1.3, spaceBetween: 16 },
-            // 768: { slidesPerView: 3, spaceBetween: 20 },
+            320: { slidesPerView: 1, spaceBetween: 20 },
+            // 420: { slidesPerView: 1},
+            768: { slidesPerView: 2, spaceBetween: 20 },
             1280: { slidesPerView: 3, spaceBetween: 20 },
             1420: { slidesPerView: 3, spaceBetween: 24 }
           },
