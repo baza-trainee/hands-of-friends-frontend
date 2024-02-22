2d34fc252d7d04434b6973f5d0c3ecba6aa82701
rodindima <rodindima1984@gmail.com>
2024-02-22 14:11:27 +0200

    fix/fix-style-hero-pagination

--- src/app/components/Hero/HeroItem.jsx
+++ src/app/components/Hero/HeroItem.jsx
@@ -13,11 +13,11 @@ export default function HeroItem({ data }) {
           className="w-full h-[1024px] xs:h-[240px] sm:h-[288px] md:h-[440px] xl:h-[685px] 2xl:h-[869px] object-cover "
         />
 
-        <p className="hidden md:block md:absolute xs:absolute md:left-1/2 md:transform -translate-x-1/2 bottom-20 mx-0 xs:text-black xs:text md:text-white text-[24px] md:w-[688px] lg:w-[881px] lg:text-[40px] xl:w-[1175px] ">
+        <p className="hidden xl:block md:absolute xs:absolute md:left-1/2 md:transform -translate-x-1/2 bottom-20 mx-0 xs:text-black xs:text md:text-white text-[24px] md:w-[688px] lg:w-[881px] lg:text-[40px] xl:w-[1175px] ">
           {data.title}
         </p>
       </li>
-      <div className="md:hidden sm:text-[24px] xs:text-[18px] xs:max-w-[288px] sm:max-w-[388px] mt-8">
+      <div className="relative md:hidden sm:text-[24px] xs:text-[18px] xs:max-w-[288px] sm:max-w-[388px] mt-8">
         {data.title}
       </div>
     </>
