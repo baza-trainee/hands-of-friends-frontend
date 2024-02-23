60c8eb6d598fdf1a913de21feced137afcef5574
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2024-02-23 15:16:51 +0200

    Revert "fix: server/client mismatch"

--- src/app/hooks/useCurrentLang.jsx
+++ src/app/hooks/useCurrentLang.jsx
@@ -1,9 +1,12 @@
 import { useCookies } from "react-cookie";
 import { cookieName } from "../i18n/settings";
-import { useRouter } from "next/navigation";
 
 export const useCurrentLang = () => {
-  const { locale, defaultLocale } = useRouter();
-  const [cookies] = useCookies(cookieName);
-  return locale || cookies.i18next || defaultLocale;
+  const [cookies, setCookie] = useCookies([cookieName]);
+
+  if ("ua" === cookies.i18next) {
+    return "ua";
+  } else {
+    return "en";
+  }
 };
