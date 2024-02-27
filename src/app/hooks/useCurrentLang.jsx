fe2363af9a4135c706f6ee26e51a839de0dde494
dav1dashka <dujadavyd17@gmail.com>
2024-02-27 20:35:44 +0200

    hotfix: translate

--- src/app/hooks/useCurrentLang.jsx
+++ src/app/hooks/useCurrentLang.jsx
@@ -3,11 +3,14 @@ import { cookieName } from "../i18n/settings";
 
 export const useCurrentLang = () => {
   const [cookies, setCookie] = useCookies([cookieName]);
-  const storedData = localStorage.getItem('i18nextLng');
-
-  if ("ua" === storedData) {
-    return "ua";
-  } else {
-    return "en";
+  if (typeof window !== 'undefined') {
+    const storedData = localStorage.getItem('i18nextLng');
+    if ("ua" === storedData) {
+      return "ua";
+    } else {
+      return "en";
+    }
   }
+
+
 };
