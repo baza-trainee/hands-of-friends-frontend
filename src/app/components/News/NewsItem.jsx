44178770f351485a1ff8ac4c39356f1a41da2657
rodindima <rodindima1984@gmail.com>
2024-02-28 15:10:18 +0200

    fix/fix-style-news-section

--- src/app/components/News/NewsItem.jsx
+++ src/app/components/News/NewsItem.jsx
@@ -3,68 +3,57 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 import NewsModal from "./NewsModal";
-import { useTranslation } from '@/app/i18n/client'
+import { useTranslation } from "@/app/i18n/client";
 
 const NewsItem = ({ data, lng }) => {
-  const { t } = useTranslation(lng, 'news')
+  const { t } = useTranslation(lng, "news");
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [formattedDescription, setFormattedDescription] = useState("");
-
+  const [formattedDescription, setFormattedDescription] = useState(
+    data.description
+  );
   useEffect(() => {
-    if (data.description) {
-      const words = data.description.split(" ");
-      const truncatedDescription = words.slice(0, 30).join(" ");
-
-      const formattedDescWithClasses = truncatedDescription
-        .replace(/<ul/g, `<ul class="your-ul-class"`)
-        .replace(/<ol/g, `<ol class="your-ol-class"`);
-
-      if (words.length > 30) {
-        setFormattedDescription(formattedDescWithClasses + "...");
-      } else {
-        setFormattedDescription(formattedDescWithClasses);
-      }
-    }
+    setFormattedDescription(data.description);
   }, [data.description]);
-
   return (
-		<div className='bg-lightBlue'>
-			<li
-				key={data.id}
-				className='flex bg-lightBlue flex-col h-[824px] xs:min-h-[630px] sm:min-h-[772px] md:min-h-[832px] lg:min-h-[821px] p-6 '
-			>
-				<Image
-					src={data.image}
-					alt='Photo of the news'
-					width={302}
-					height={400}
-					className='h-[264px] xs:w-[256px] sm:w-[340px] md:w-[283px] lg:w-full object-cover mb-6 '
-				/>
+    <div className="bg-lightBlue">
+      <li
+        key={data.id}
+        className="flex bg-lightBlue flex-col h-[790px] sm:h-[772px] md:min-h-[832px] lg:min-h-[836px] 2xl:min-h-[821px] p-4 sm:p-6 "
+      >
+        <Image
+          src={data.image}
+          alt="Photo of the news"
+          width={302}
+          height={400}
+          className="h-[238px] sm:h-[316px] md:h-[264px] xs:w-[256px] sm:w-[340px] md:w-[283px] lg:w-full object-cover mb-6 "
+        />
+        <span className="mb-2 text-xl lg:text-lg">{data.date}</span>
+        <h3 className="text-xl font-bold mb-2">{data.title}</h3>
 
-				<h3 className='text-xl font-bold mb-2'>{data.title}</h3>
-				<span className='mb-2 text-xl lg:text-lg'>{data.date}</span>
+        <div className="line-clamp-[9] sm:line-clamp-[7] md:line-clamp-[8]">
+          <p
+            className="flex-1"
+            dangerouslySetInnerHTML={{ __html: formattedDescription }}
+          ></p>
+        </div>
 
-				<p
-					className='flex-1'
-					dangerouslySetInnerHTML={{ __html: formattedDescription }}
-				></p>
-
-				<button
-					onClick={() => setModalIsOpen(true)}
-					type='button'
-					className=' block min-w-[198px] min-h-[60px] mx-auto rounded-md underline underline-offset-4 px-0 mt-8 font-normal bg-transperent text-black border border-transparent hover:text-deepBlue hover:border hover:border-deepBlue md:mt-10 xl:mt-8'
-				>
-					{t('btn')}
-				</button>
-				<NewsModal
-					isOpen={modalIsOpen}
-					onClose={() => setModalIsOpen(false)}
-					data={data}
-					description={data.description}
-				/>
-			</li>
-		</div>
-	)
+        <button
+          onClick={() => setModalIsOpen(true)}
+          type="button"
+          style={{ marginTop: "auto" }}
+          className="text-xl block min-w-[198px] min-h-[60px] mx-auto rounded-md underline underline-offset-4 px-0 mt-8 font-normal bg-transperent text-black border border-transparent hover:border hover:border-deepBlue md:mt-10 xl:mt-8 "
+        >
+          {t("btn")}
+        </button>
+        <NewsModal
+          isOpen={modalIsOpen}
+          onClose={() => setModalIsOpen(false)}
+          data={data}
+          description={data.description}
+        />
+      </li>
+    </div>
+  );
 };
 
 export default NewsItem;
--- src/app/components/News/NewsModal.jsx
+++ src/app/components/News/NewsModal.jsx
@@ -15,24 +15,30 @@ const NewsModal = ({ isOpen, onClose, data, formattedDescription }) => {
   return (
     <Modal
       isOpen={isOpen}
-      className="news-modal w-[288px] sm:w-[344px] md:w-[586px] xl:w-[740px] 2xl:w-[792px] flex flex-col "
+      className="news-modal h-[906px] w-[288px] sm:w-[344px] md:w-[586px] xl:w-[740px] 2xl:w-[792px] flex flex-col "
       overlayClassName="news-overlay"
       onRequestClose={onClose}
       contentLabel="Детальна інформація про новину"
+      ariaHideApp={false}
     >
       {" "}
-      <button className="mb-4 self-end justify-end" onClick={onClose}>
+      <button
+        className="mb-2 self-end justify-end sm:mr-[-8px] md:mr-[1px] 2xl:mr-2"
+        onClick={onClose}
+      >
         <Close />
       </button>
-      <div className="modal-content md:w-[516px] xl:w-[660px] 2xl:w-[700px] mx-auto custom-scrollbar [&::-webkit-scrollbar]:[width:7px] md:[&::-webkit-scrollbar]:[width:8px] xl:[&::-webkit-scrollbar]:[width:10px]">
+      <div className="modal-content md:w-[529px] xl:w-[680px] xl:mr-[10px] sm:pr-[8px] xl:pr-[20px] 2xl:w-[725px] mr-auto custom-scrollbar [&::-webkit-scrollbar]:[width:6px] md:[&::-webkit-scrollbar]:[width:8px] xl:[&::-webkit-scrollbar]:[width:10px]">
         <Image
           src={data.image}
           alt="Photo of the news"
           width={696}
           height={315}
-          className="mx-auto mb-6 w-[230px] xs:h-[175px] xs:w-[288px] md:w-[506px] md:h-[409px] xl:w-[660px] 2xl:w-[696px] 2xl:h-[386px] object-cover "
+          className="mb-6 w-[256px] h-[175px] sm:h-[198px] sm:w-[288px] md:w-[506px] md:h-[336px] xl:h-[409px] xl:w-[660px] 2xl:w-[696px] 2xl:h-[386px] object-cover "
         />
-        <span className="text-xl lg:text-lg mb-4">{data.date}</span>
+        <span className="text-base sm:text-lg md:text-xl lg:text-lg mb-4">
+          {data.date}
+        </span>
         <h2 className="text-xl font-bold mb-3 md:max-w-[506px]">
           {data.title}
         </h2>
