ed1a2733b7474414a2feaf94e1229226d93d9a8f
Rodin <Rodindima1984@gmail.com>
2024-03-02 17:44:28 +0200

    fix/fix=style-hero-section

--- src/app/components/Hero/HeroItem.jsx
+++ src/app/components/Hero/HeroItem.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react'
+import Image from 'next/image'
 
 export default function HeroItem({ data }) {
-  return (
-    <>
-      <li className="bg-[#E0F2FE]">
-        <Image
-          src={data.image}
-          alt={`${data.image}`}
-          width={1440}
-          height={869}
-          priority={true}
-          className="w-[1440px] h-[1024px] xs:h-[240px] sm:h-[288px] md:h-[440px] xl:h-[685px] 2xl:h-[869px] object-cover "
-        />
+	return (
+		<>
+			<li className='bg-[#E0F2FE]'>
+				<Image
+					src={data.image}
+					alt={`${data.image}`}
+					width={1440}
+					height={869}
+					priority={true}
+					className='w-[1440px] h-[1024px] xs:h-[240px] sm:h-[288px] md:h-[440px] xl:h-[685px] 2xl:h-[869px] object-cover '
+				/>
 
-        <p className="hidden xl:block md:absolute xs:absolute md:left-1/2 md:transform -translate-x-1/2 bottom-20 mx-0 xs:text-black xs:text md:text-white text-[24px] md:w-[688px] lg:w-[881px] lg:text-[40px] xl:w-[1175px] ">
-          {data.title}
-        </p>
-      </li>
-      <div className="relative md:hidden sm:text-[24px] xs:text-[18px] xs:max-w-[288px] sm:max-w-[388px] mt-8">
-        {data.title}
-      </div>
-    </>
-  );
+				<p className='hidden xl:block md:absolute xs:absolute md:left-1/2 md:transform -translate-x-1/2 bottom-[144px] mx-0 xs:text-black xs:text md:text-white text-[24px] md:w-[688px] lg:w-[881px] lg:text-[40px] xl:w-[1175px] '>
+					{data.title}
+				</p>
+			</li>
+			<div className='relative md:hidden sm:text-[24px] xs:text-[18px] xs:max-w-[288px] sm:max-w-[388px] mt-8'>
+				{data.title}
+			</div>
+		</>
+	)
 }
