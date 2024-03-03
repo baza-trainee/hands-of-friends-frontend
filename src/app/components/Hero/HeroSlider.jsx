e3f0d6b562492178568e2b66fc05ce7c33f4a668
Rodin <Rodindima1984@gmail.com>
2024-03-03 16:25:17 +0200

    fix/fix-style-hero-section

--- src/app/components/Hero/HeroSlider.jsx
+++ src/app/components/Hero/HeroSlider.jsx
@@ -1,37 +1,37 @@
-"use client";
+'use client'
 
-import React from "react";
-import Link from "next/link";
-import { useTranslation } from "@/app/i18n/client";
-import UniversalSlider from "../ UniversalSlider";
-import HeroItem from "./HeroItem";
-import Container from "../Container";
+import React from 'react'
+import Link from 'next/link'
+import { useTranslation } from '@/app/i18n/client'
+import UniversalSlider from '../ UniversalSlider'
+import HeroItem from './HeroItem'
+import Container from '../Container'
 
 const HeroSlider = ({ lng }) => {
-  const { t } = useTranslation(lng, "hero");
-  return (
-    <>
-      <div className="HeroSlider max-w-[1440px] mb-6 md:mb-0 relative mx-auto">
-        <UniversalSlider
-          endpoint="hero-slider/"
-          ItemComponent={HeroItem}
-          skeletonType="hero"
-          useBullets={true}
-          swiperSettings={{}}
-        />
-      </div>
-      <Container>
-        <Link
-          href="https://www.monobank.ua/?lang=uk"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="block py-4 text-lg text-center text-white transition rounded cursor-pointer border-violet bg-violet w-full md:hidden"
-        >
-          {t("btn")}
-        </Link>
-      </Container>
-    </>
-  );
-};
+	const { t } = useTranslation(lng, 'hero')
+	return (
+		<>
+			<div className='HeroSlider max-w-[1440px] mb-6 md:mb-0 relative mx-auto'>
+				<UniversalSlider
+					endpoint='hero-slider/'
+					ItemComponent={HeroItem}
+					skeletonType='hero'
+					useBullets={true}
+					swiperSettings={{}}
+				/>
+			</div>
+			<Container>
+				<Link
+					href='https://www.monobank.ua/?lang=uk'
+					target='_blank'
+					rel='noopener noreferrer'
+					className='block py-4 text-lg text-center text-white transition rounded cursor-pointer border-2 border-violet bg-violet hover:bg-transparent hover:text-violet hover:border-violet w-full md:hidden'
+				>
+					{t('btn')}
+				</Link>
+			</Container>
+		</>
+	)
+}
 
-export default HeroSlider;
+export default HeroSlider
