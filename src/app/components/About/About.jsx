2ec6ef0ce26bb7f1558ede6598caebab1d968714
Rodin <Rodindima1984@gmail.com>
2024-03-16 05:00:43 +0200

    fix:fix-bugs-tenders-aboutUs-pages

--- src/app/components/About/About.jsx
+++ src/app/components/About/About.jsx
@@ -6,131 +6,108 @@ import { useRouter } from 'next/navigation'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Pagination, Keyboard, A11y, Mousewheel } from 'swiper/modules'
 import { useTranslation } from '@/app/i18n/client'
-import { motion } from 'framer-motion'
 
-import { useCurrentLang } from "@/app/hooks/useCurrentLang";
+import { useCurrentLang } from '@/app/hooks/useCurrentLang'
 
 import Section from '../Section'
 import Container from '../Container'
 import Title from '../Title'
 import Action from '../Action'
 
-import Team from '../../../../public/img/about-us1.png'
+import Team from '../../../../public/img/about-us.png'
+import Cargo from '../../../../public/img/cargo.png'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
-const aboutAnimation = {
-	hidden: { opacity: 0 },
-	visible: { opacity: 1 },
-}
 export default function About({ lng }) {
-	const { t } = useTranslation(lng, 'about-section');
-	const router = useRouter();
-	const lang = useCurrentLang() === 'uk' ? 'ua' : 'en';
+	const { t } = useTranslation(lng, 'about-section')
+	const router = useRouter()
+	const lang = useCurrentLang() === 'uk' ? 'ua' : 'en'
 
 	return (
-		<motion.div
-			initial='hidden'
-			whileInView='visible'
-			viewport={{ amount: 0.2 }}
-		>
-			<Section>
-				<Container>
-					<blockquote>
-						<motion.p
-							variants={aboutAnimation}
-							transition={{ duration: 0.7 }}
-							className='min-w-[288px] xs:max-w-[360px] md:max-w-[608px] text-xs italic mb-8 font-bold sm:text-sm md:text-2xl lg:text-left xl:text-left'
-						>
-							"{t('blockquote')}"
-						</motion.p>
-					</blockquote>
+		<Section>
+			<Container>
+				<blockquote>
+					<p className='min-w-[288px] xs:max-w-[360px] md:max-w-[608px] text-xs italic mb-8 font-bold sm:text-sm md:text-2xl lg:text-left xl:text-left'>
+						"{t('blockquote')}"
+					</p>
+				</blockquote>
 
-					<div className='md:hidden'>
-						<Swiper
-							pagination={{
-								clickable: true,
-								type: 'progressbar',
-							}}
-							modules={[Keyboard, Pagination, A11y, Mousewheel]}
-							className='swiper about '
-							breakpoints={{
-								320: {
-									slidesPerView: 1,
-								},
-							}}
-						>
-							<SwiperSlide>
-								<div className=''>
-									<Image
-										src={Team}
-										width={388}
-										height={254}
-										alt='Picture of the our team'
-										className='object-cover'
-									/>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide>
-								<div className=''>
-									<Image
-										src={Team}
-										width={388}
-										height={254}
-										alt='Picture of the our team'
-									/>
-								</div>
-							</SwiperSlide>
-						</Swiper>
-					</div>
+				<div className='md:hidden'>
+					<Swiper
+						pagination={{
+							clickable: true,
+							type: 'progressbar',
+						}}
+						modules={[Keyboard, Pagination, A11y, Mousewheel]}
+						className='swiper about '
+						breakpoints={{
+							320: {
+								slidesPerView: 1,
+							},
+						}}
+					>
+						<SwiperSlide>
+							<div className=''>
+								<Image
+									src={Team}
+									width={388}
+									height={254}
+									alt='Picture of the our team'
+									className='object-cover'
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							<div className=''>
+								<Image
+									src={Cargo}
+									width={388}
+									height={254}
+									alt='Picture of the our cargo'
+								/>
+							</div>
+						</SwiperSlide>
+					</Swiper>
+				</div>
 
-					<div className='hidden md:flex flex-col	gap-5 mb-10 xl:flex-row	'>
-						<motion.div
-							variants={aboutAnimation}
+				<div className='hidden md:flex flex-col	gap-5 mb-10 xl:flex-row	'>
+					<Image
+						src={Team}
+						width='auto'
+						height={398}
+						alt='Picture of the our team'
+						className='md:h-[386px] w-[688px] lg:mx-0 xl:w-[550px] 2xl:w-[588px]'
+					/>
 
-						>
-							<Image
-								src={Team}
-								width='auto'
-								height={398}
-								alt='Picture of the our team'
-								className='md:h-[386px] w-[688px] lg:mx-0 xl:w-[550px] 2xl:w-[588px]'
-							/>
-						</motion.div>
-						<motion.div
-							variants={aboutAnimation}
+					<Image
+						src={Cargo}
+						width='auto'
+						height={398}
+						alt='Picture of the our team'
+						className='md:h-[386px] w-[688px] lg:mx-0 xl:w-[550px] 2xl:w-[588px]'
+					/>
+				</div>
 
-						>
-							<Image
-								src={Team}
-								width='auto'
-								height={398}
-								alt='Picture of the our team'
-								className='md:h-[386px] w-[688px] lg:mx-0 xl:w-[550px] 2xl:w-[588px]'
-							/>
-						</motion.div>
-					</div>
-
-					<Title className='mb-15' titleName={t('title')} />
-					<h2 className='text-xl mb-8 md:text-2xl'>{t('subtitle')}</h2>
-					<div className='text-base max-w-[740px] md:text-lg '>
-						<p className='mb-4'>{t('p1')}</p>
-						<p className='mb-4'>{t('p2')}</p>
-						<p className='mb-8'>{t('p3')}</p>
-					</div>
-					<div className='flex justify-center md:justify-start'>
-						<Action
-							onClick={() => router.push(`${lang}/aboutUs`)}
-							type='button'
-							className='text-center px-0 min-w-[12.38rem] font-normal bg-deepBlue border border-transparent hover:text-deepBlue hover:border hover:border-deepBlue'
-						>
-							{t('btn')}
-						</Action>
-					</div>
-				</Container>
-			</Section>
-		</motion.div>
+				<Title className='mb-15' titleName={t('title')} />
+				<h2 className='text-xl mb-8 md:text-2xl'>{t('subtitle')}</h2>
+				<div className='text-base max-w-[740px] md:text-lg '>
+					<p className='mb-4'>{t('p1')}</p>
+					<p className='mb-4'>{t('p2')}</p>
+					<p className='mb-8'>{t('p3')}</p>
+				</div>
+				<div className='flex justify-center md:justify-start'>
+					<Action
+						onClick={() => router.push(`${lang}/aboutUs`)}
+						type='button'
+						className='text-center px-0 min-w-[12.38rem] font-normal bg-deepBlue border border-transparent hover:text-deepBlue hover:border hover:border-deepBlue'
+					>
+						{t('btn')}
+					</Action>
+				</div>
+			</Container>
+		</Section>
 	)
 }
