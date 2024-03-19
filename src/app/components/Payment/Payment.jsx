ffb00bb7259696d5f5f178614759541004d28fd2
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-03-19 13:01:12 +0100

    feat: add adaptivity

--- src/app/components/Payment/Payment.jsx
+++ src/app/components/Payment/Payment.jsx
@@ -30,11 +30,20 @@ export const Payment = ({ lng, className }) => {
           onClose={() => toggleModal()}
           className="bg-stone-900 bg-opacity-30 backdrop-blur-[10px]"
         >
-          <div className="relative w-288px p-8 text-sm text-black text-center bg-[#F1F5F9] rounded-[5px]">
-            <button type="button" onClick={toggleModal}>
+          <div
+            className="relative w-[288px] px-[8.5px] py-8 text-sm text-black text-center bg-[#F1F5F9] rounded-[5px]
+          sm:w-[388px] sm:p-8
+          md:w-[522px]
+          xl:w-[557px]"
+          >
+            <button
+              type="button"
+              onClick={toggleModal}
+              className="absolute  top-[10px] right-[10px]  w-6 h-6 cursor-pointer"
+            >
               <HiPlus
                 size={24}
-                className="absolute transition transform rotate-45 cursor-pointer top-[10px] right-[10px] fill-slate-900  hover:fill-violet hover:scale-110"
+                className="absolute transition transform rotate-45 cursor-pointer top-0 right-0 fill-slate-900  hover:fill-violet hover:scale-110"
               />
             </button>
             <PaymentBody lng={lng} />
--- src/app/components/Payment/PaymentBody.jsx
+++ src/app/components/Payment/PaymentBody.jsx
@@ -5,9 +5,16 @@ import { useTranslation } from "@/app/i18n/client";
 export const PaymentBody = ({ lng }) => {
   const { t } = useTranslation(lng, "payment");
   return (
-    <div>
-      <h2 className="mb-6 font-semibold"> {t("title")}</h2>
-      <ul className="mb-2">
+    <div className="text-sm sm:text-base md:text-lg xl:text-xl">
+      <h2
+        className="m-auto mb-6 max-w-[223px] font-semibold text-center antialiased
+      sm:max-w-[320px] sm:text-xl
+      md:max-w-[400px] md:text-2xl md:mb-8
+      xl:max-w-[493px] xl:text-3xl"
+      >
+        {t("title")}
+      </h2>
+      <ul className="mb-2 flex flex-col gap-1 md:mb-4">
         <li>
           <p> {t("EDRPOU")}</p>
         </li>
@@ -18,9 +25,9 @@ export const PaymentBody = ({ lng }) => {
           <p> {t("BankName")}</p>
         </li>
       </ul>
-      <p className="font-bold"> {t("PurposeOfPayment")}</p>
+      <p className="mb-1 font-bold"> {t("PurposeOfPayment")}</p>
       <p className="mb-6"> {t("text")}</p>
-      <Action className="mb-6 min-w-[184px] text-white  bg-violet border-violet hover:bg-transparent hover:text-violet transition border-2 rounded cursor-pointer">
+      <Action className="mb-6 min-w-[184px] text-lg text-white  bg-violet border-violet hover:bg-transparent hover:text-violet transition border-2 rounded cursor-pointer">
         {t("copyIBAN")}
       </Action>
       <p className="font-bold"> {t("gratitude")}</p>
