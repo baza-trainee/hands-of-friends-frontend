642fd1204036cdc9b14edeba2314d703f2ba17c6
dav1dashka <dujadavyd17@gmail.com>
2024-03-20 18:14:07 +0200

    test

--- src/app/i18n/settings.js
+++ src/app/i18n/settings.js
@@ -1,5 +1,5 @@
 export const fallbackLng = "ua";
-export const languages = [fallbackLng, "en"];
+export const languages = [fallbackLng, "ua"];
 export const defaultNS = "translation";
 export const cookieName = "i18next";
 
